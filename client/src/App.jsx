f615453db44ffad2cb8692829a9173cfa08c2f19
Darseen <105995943+Darseen@users.noreply.github.com>
2023-05-20 18:23:36 +0300

    Replaced navbar with a sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,16 @@
-import { Routes, Route, useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { Routes, Route } from "react-router-dom";
 
 /* COMPONENTS IMPORT */
 import Home from "./components/Home";
 import Chats from "./components/Chats";
 import Register from "./components/Register";
+import Login from "./components/Login";
 
 import "./App.css";
-import Login from "./components/Login";
-import Navbar from "./components/Navbar";
 
 const App = () => {
-  const user = useSelector((state) => state.user);
-
-  if (!user) {
-    // useNavigate("/");
-  }
-
   return (
     <>
-      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
